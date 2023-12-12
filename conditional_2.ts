7e4122fd95a3db26a8ea6af987600d68a5b6cf22
Rachel E <117534780+rachel-easter@users.noreply.github.com>
2023-12-12 13:51:30 +0530

    Create conditional_2.ts

---
+++ conditional_2.ts
@@ -0,0 +1,29 @@
+///switch case:
+//syntax:switch(condition){ case 1:break; default: break;}
+//Example-1:
+//let money:number=1500;
+let money:number=300;
+
+switch(money){
+    case 1400:
+        console.log("No need to pay tax");
+        break;
+    case 1500:
+        console.log("need to pay tax");
+        break;
+    case 300:
+        console.log("low balance");
+        console.log("add money to card");
+    case 2:
+        console.log("two cases executed !");
+        break;
+    default:
+        console.log("out of range");
+        break;
+        
+}  
+
+
+
+
+    
